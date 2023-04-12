cf974fe103ee8fafcc64fd9c5e51d1f407f1b84e
namtran6701 <nam3864779@gmail.com>
2023-04-11 22:14:53 -0400

    LM demo

---
+++ lm_demo.R
@@ -0,0 +1,39 @@
+
+
+# Linear Regression
+
+x = round(rnorm(50, 20, 3))
+
+y = runif(50, 4.5, 7)
+
+test = round(rnorm(50, 19, 4))
+
+df <- data.frame(Age = x,
+                 Height = y)
+
+
+## Run a linear regression model 
+
+model <- lm(data = df, 
+            Height ~ Age)
+
+summary(model)
+
+
+## Compute Model's metrics 
+
+library(performance)
+
+cat("RMSE of the model", performance_rmse(model))
+
+cat("MAE of the model", performance_mae(model))
+
+## Predict 
+
+top_prediction <- head(predict(model, 
+             newdata = data.frame(Age = test)), 10)
+
+(predict_table <- data.frame(Age = head(test, 10),
+                             Height_predicted = top_prediction))
+
+  
